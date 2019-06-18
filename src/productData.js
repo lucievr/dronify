e365221afbaf4f6d44ac7c2df829f029c8247ba2
Lucie <46979603+lucievr@users.noreply.github.com>
2019-06-18 12:35:40 +0200

    Add product 3 specs

--- src/productData.js
+++ src/productData.js
@@ -19,8 +19,7 @@ const Mavic2 = {
 "https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic2-zoom.png?alt=media&token=e14db9dc-2014-4cff-8980-e75525333ca1",
 "https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic2i.jpg?alt=media&token=d42e62a6-0079-4451-b365-4022d02d5298",
 "https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic2ii.jpg?alt=media&token=b12d11c4-af7d-427a-92a6-81cf91c3f5c4",
-"https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic2iii.jpg?alt=media&token=45e61e1c-9d81-475b-93c3-f216198d4452"
-],
+"https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic2iii.jpg?alt=media&token=45e61e1c-9d81-475b-93c3-f216198d4452"],
 }
 
 const MavicAir = {
@@ -44,9 +43,51 @@ const MavicAir = {
 "https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic-air-black-fold.png?alt=media&token=6cc468de-c4b1-40e9-9d67-c63bd7b24cc0",
 "https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic-air-white-fold.png?alt=media&token=aa1063ba-02dd-456a-bca1-d2078501a587",
 "https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic-air-white-unfold.png?alt=media&token=e1048ec1-8959-4846-b82d-62d80f1b9956",
-"https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavicair.jpg?alt=media&token=9d3d4b62-c7f6-4e26-a454-dcc8a53645f8"
-],
+"https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavicair.jpg?alt=media&token=9d3d4b62-c7f6-4e26-a454-dcc8a53645f8"],
 }
+
+const MavicProPlatinum = {
+    name: "Mavic Pro Platinum",
+    price: 999,
+    description: "With enhanced endurance and quieter flight, the Mavic Pro Platinum is DJI's best portable drone yet.",
+    icon1: "/images/mavicproicon1.png",
+    spec1: {"Flight time": "30 MINS"},
+    icon2: "/images/mavicproicon2.png",
+    spec2: {"Control range": "7 KM"},
+    icon3: "/images/mavicproicon3.png",
+    spec3: {"Noise reduction": "4 dB"},
+    icon4: "/images/mavicproicon4.png",
+    spec4: {"Gimbal": "3-axis"},
+    icon5: "/images/mavicproicon5.png",
+    spec5: {"Video resolution": "4K"},
+    icon6: "/images/mavicproicon6.png",
+    spec6: {"Camera resolution": "12 MP"},
+    imageURL: ["https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic-pro-platinum.png?alt=media&token=8e983e0b-e664-4d4b-97ce-0dc9908cf952", 
+    "https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic-pro-platinum-small.png?alt=media&token=79437ccd-09bb-4995-b94c-3de66b839477",
+    "https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic-pro-i.jpg?alt=media&token=2c451b11-a515-489e-8db5-d8591d7beb08", 
+    "https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic-pro-ii.jpg?alt=media&token=4d38ceec-6940-4c6c-84ed-a6dc7272e771", 
+    "https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/consumer-drones%2Fmavic-pro-iii.jpg?alt=media&token=d8064924-4499-481e-ac38-70d38e909aa0"],
+}
+
+const Phantom3SE = {
+    name: "Phantom 3 SE",
+    price: 699,
+    description: "The Phantom 3 SE is the most streamlined addition to the Phantom 3 series yet. It connects to the DJI GO app via Wi-Fi with maximum a video transmission range of up to 2.5 mi (4 km), and it has a 4K video camera. ",
+    icon1: "/images/phantom3icon1.png",
+    spec1: {"Flight time": "30 MINS"},
+    icon2: "/images/phantom3icon2.png",
+    spec2: {"Control range": "7 KM"},
+    icon3: "/images/phantom3icon3.png",
+    spec3: {"Speed": "4 dB"},
+    icon4: "/images/phantom3icon4.png",
+    spec4: {"Video resolution": "3-axis"},
+    icon5: "/images/phantom3icon5.png",
+    spec5: {"Sensor range": "4K"},
+    icon6: "/images/phantom3icon6.png",
+    spec6: {"Camera resolution": "12 MP"},
+    imageURL: [],
+}
+
 
 
 
