cbe1cfb4bd81204eae342970c0cdfa9a8b77f5fe
Miguel Osorio <mial.osorio@gmail.com>
2019-06-18 10:47:00 +0200

    once again

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
 import { Global } from "@emotion/core"
-
 import GlobalStyles from "../components/GlobalStyles"
-import Layout from "../components/layout"
-// import Image from "../components/image"
 import SEO from "../components/seo"
-
 import LandingPage from '../components/LandingPage/landingPage'
 
 const IndexPage = () => (
@@ -14,9 +9,6 @@ const IndexPage = () => (
     <Global styles={GlobalStyles} />
     <LandingPage />
     <SEO title="home" />
-    {/*  <Layout>
-      <Link to="/page-2/">Go to page 2</Link>
-    </Layout> */}
   </>
 )
 
