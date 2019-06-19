7a155dd31dde078d92d99373327ef25a8bc6004d
Lucie <46979603+lucievr@users.noreply.github.com>
2019-06-19 19:25:01 +0200

    Remove material-ui

--- src/components/Products/Consumers.js
+++ src/components/Products/Consumers.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { css } from "@emotion/core"
 import { Link } from "gatsby"
-import Button from "@material-ui/core/Button"
 
 const Layout = css`
   height: 100%;
@@ -13,7 +12,7 @@ const Consumers = () => {
     <div css={Layout}>
       <h3>Consumer drones</h3>
       <Link to="/consumer-drones/">
-        <Button variant="outlined">Explore our collection</Button>
+        <button>Explore our collection</button>
       </Link>
     </div>
   )
