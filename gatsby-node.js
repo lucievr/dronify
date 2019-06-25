a2db3080a77f85d2541d37db1f68bc65c4c615f7
Lucie <46979603+lucievr@users.noreply.github.com>
2019-06-25 14:12:00 +0200

    Install auth0 + make  webpack ignore auth0 at build time :construction:

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,4 +17,27 @@ exports.onCreatePage = async ({ page, actions }) => {
       // Update the page.
       createPage(page)
     }
+}
+
+      /* During the build step, `auth0-js` will break because it relies on
+       ** browser-specific APIs. Fortunately, we don’t need it during the build.
+       ** Using Webpack’s null loader, we’re able to effectively ignore `auth0-js`
+       ** during the build. (See `src/utils/auth.js` to see how we prevent this
+       ** from breaking the app.)
+       */
+      
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html") {
+
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /auth0-js/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
   }
