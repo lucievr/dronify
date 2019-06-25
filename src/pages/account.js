416f6a18a050dd89c2d4134535bf7e21b60aeba3
Lucie <46979603+lucievr@users.noreply.github.com>
2019-06-25 13:46:42 +0200

    Install Reach router, override Gatsby's onCreatePage func + create home, billing and settings comps

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,16 +1,31 @@
 import React from "react"
+import { Router } from "@reach/router"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Global } from "@emotion/core"
 import GlobalStyles from "../components/GlobalStyles"
 
+const Home = () => <p>Home</p>
+const Settings = () => <p>Settings</p>
+const Billing = () => <p>Billing</p>
 
 const Account = () => (
-    <Layout>
-        <Global styles={GlobalStyles} />
-        <SEO title="user account" />
-        <h2>This will be the protected route for user account.</h2>
-    </Layout>
+  <Layout>
+    <Global styles={GlobalStyles} />
+    <SEO title="user account" />
+    <nav>
+      <Link to="/account">Home</Link>{" "}
+      <Link to="/account/settings">Settings</Link>{" "}
+      <Link to="/account/billing">Billing</Link>{" "}
+    </nav>
+    <h2>This will be the protected route for user account.</h2>
+    <Router>
+      <Home path="/account" />
+      <Settings path="/account/settings" />
+      <Billing path="/account/billing" />
+    </Router>
+  </Layout>
 )
 
 export default Account
