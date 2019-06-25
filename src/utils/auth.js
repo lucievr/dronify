038cf53e1b66367d4f879906a45a205fee1fb9d0
Lucie <46979603+lucievr@users.noreply.github.com>
2019-06-25 16:17:07 +0200

    Add silent auth and update gatsby browser apis

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -67,3 +67,8 @@ export const handleAuthentication = () => {
 export const getProfile = () => {
   return user
 }
+
+export const silentAuth = callback => {
+    if (!isAuthenticated()) return callback()
+    auth.checkSession({}, setSession(callback))
+  }
