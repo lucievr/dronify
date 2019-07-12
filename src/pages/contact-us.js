bc3ff4bf0e39fcafba79736d3c90cf11f0dfcde7
Marijan3062 <brailo.marijan@gmail.com>
2019-07-12 16:19:57 +0200

    added contact form, needs styling

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -4,15 +4,16 @@ import Navigation from "../components/main-page/landing/navigation/Navigation"
 import GlobalStyles from "../components/styles/GlobalStyles"
 import { Global } from "@emotion/core"
 import SEO from "../components/gatsby-default-files/seo"
+import ContactForm from "../components/form/contact-form"
 
 // import { connect } from 'react-redux'
 
 const ContactUs = () => {
-
     return (
         <Layout>
             <Navigation />
             <Global styles={GlobalStyles} />
+            <ContactForm />
             <SEO title="Contact Us" />
         </Layout>
     )
