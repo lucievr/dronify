2fd9bc5359baba0b389895f02985072f45829992
Lucie <46979603+lucievr@users.noreply.github.com>
2019-07-16 12:32:21 +0200

    Delivery component form static

--- src/components/cart/layout/DeliveryForm.js
+++ src/components/cart/layout/DeliveryForm.js
@@ -1,8 +1,10 @@
 import React from "react"
-import { Col, Button, Form, FormGroup, Label, Input } from "reactstrap"
+import { Col, Row, Button, Form, FormGroup, Label, Input } from "reactstrap"
 import { Global } from "@emotion/core"
 import { Link } from "gatsby"
 import GlobalStyles from "../../styles/GlobalStyles"
+import { login, isAuthenticated, getProfile } from "../../../utils/auth"
+import { navigate } from "gatsby"
 
 import {
     wrapper,
@@ -17,9 +19,45 @@ import {
 class DeliveryForm extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        // let user = getProfile()
+        this.state = {
+            user: {
+                email: "",
+                name: "",
+                picture: "",
+                address: "Taborska 31",
+                address2: "",
+                city: "Prague",
+                country: "Czech Republic",
+                postcode: "140 00",
+            },
+        }
+    }
+
+    handleChange = event => {
+        const { user } = { ...this.state }
+        const currentState = user
+        const { name, value } = event.target
+        currentState[name] = value
+
+        this.setState({ user: currentState })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        try {
+            navigate("/payment/")
+            this.setState({})
+        } catch (error) {
+            alert(error)
+        }
     }
     render() {
+        if (!isAuthenticated()) {
+            login()
+            return <p>Redirecting to login...</p>
+        }
+
         return (
             <section css={wrapper}>
                 <Global styles={GlobalStyles} />
@@ -37,12 +75,121 @@ class DeliveryForm extends React.Component {
                             <span>DELIVERY </span>
                         </span>
                     </div>
-                    <Link to="/cart/">
-                        <Button size="lg">Previous Step</Button>
-                    </Link>
-                    <Link to="/payment/">
-                        <Button size="lg">Next Step</Button>
-                    </Link>
+
+                    <Form onSubmit={this.handleSubmit}>
+                        <Row form>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="name">Full Name</Label>
+                                    <Input
+                                        type="text"
+                                        name="name"
+                                        id="name"
+                                        value={this.state.user.name}
+                                        onChange={this.handleChange}
+                                        required
+                                    />
+                                </FormGroup>
+                            </Col>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="email">Email</Label>
+                                    <Input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        value={this.state.user.email}
+                                        onChange={this.handleChange}
+                                        required
+                                    />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+
+                        <FormGroup>
+                            <Label for="address">Address</Label>
+                            <Input
+                                type="text"
+                                name="address"
+                                id="address"
+                                value={this.state.user.address}
+                                onChange={this.handleChange}
+                                required
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="exampleAddress2">
+                                Address Description (optional)
+                            </Label>
+                            <Input
+                                type="text"
+                                name="address2"
+                                id="exampleAddress2"
+                                placeholder="Apartment, studio, or floor"
+                                value={this.state.user.address2}
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
+                        <Row form>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="exampleCity">City</Label>
+                                    <Input
+                                        type="text"
+                                        name="city"
+                                        id="exampleCity"
+                                        value={this.state.user.city}
+                                        onChange={this.handleChange}
+                                        required
+                                    />
+                                </FormGroup>
+                            </Col>
+                            <Col md={4}>
+                                <FormGroup>
+                                    <Label for="country">Country</Label>
+                                    <Input
+                                        type="text"
+                                        name="country"
+                                        id="country"
+                                        value={this.state.user.country}
+                                        onChange={this.handleChange}
+                                        required
+                                    />
+                                </FormGroup>
+                            </Col>
+                            <Col md={2}>
+                                <FormGroup>
+                                    <Label for="postcode">Post Code</Label>
+                                    <Input
+                                        type="text"
+                                        name="postcode"
+                                        id="postcode"
+                                        value={this.state.user.postcode}
+                                        onChange={this.handleChange}
+                                        required
+                                    />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <FormGroup check>
+                            <Input
+                                type="checkbox"
+                                name="check"
+                                id="exampleCheck"
+                                required
+                            />
+                            <Label for="exampleCheck" check>
+                                The above details are correct
+                            </Label>
+                        </FormGroup>
+                        <Link to="/cart/">
+                            <Button size="lg">Previous Step</Button>
+                        </Link>
+
+                        <Button size="lg" type="submit">
+                            Next Step
+                        </Button>
+                    </Form>
                 </div>
             </section>
         )
